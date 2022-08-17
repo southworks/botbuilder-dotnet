b0daa7760cb7cad90e8b0b6442ba9becff4aec94
sw-joelmut <joel.mut@southworks.com>
2022-08-17 14:45:29 -0300

    Add condition before loading the resource

--- libraries/Microsoft.Bot.Builder.Dialogs/DialogContainer.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/DialogContainer.cs
@@ -98,6 +98,17 @@ namespace Microsoft.Bot.Builder.Dialogs
         }
 
         /// <summary>
+        /// Finds a child dialog that was previously added to the container. Use DialogContext as fallback to gather the dialog.
+        /// </summary>
+        /// <param name="dc">The dialog context fallback where to find the dialog.</param>
+        /// <param name="dialogId">The ID of the dialog to lookup.</param>
+        /// <returns>The Dialog if found; otherwise null.</returns>
+        public virtual Dialog FindDialog(DialogContext dc, string dialogId)
+        {
+            return Dialogs.Find(dialogId) ?? dc?.Dialogs?.Find(dialogId);
+        }
+
+        /// <summary>
         /// Called when an event has been raised, using `DialogContext.emitEvent()`, by either the current dialog or a dialog that the current dialog started.
         /// </summary>
         /// <param name="dc">The dialog context for the current turn of conversation.</param>
